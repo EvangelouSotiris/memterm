f7782806fc398d4c0b92830e99485392c4a982c3
EvangelouSotiris <swtevag@gmail.com>
2020-04-17 15:01:37 +0300

    better refreshing, more accurate info

--- core.sh
+++ core.sh
@@ -35,43 +35,48 @@ print_bars() {
     done
     echo "$hashtags"
 }
+printf "Welcome to ${PURPLE}Memterm${NC}!\n"
+printf "Memterm is simple live tool showing useful system information. Continue?[${GREEN}y${NC}/${RED}n${NC}]\n"
 
-tput clear
+read answer
+if [ $answer != "y" ]; then
+	echo "Exiting.."
+fi
 
-. /etc/os-release
+echo "Welcome! Memterm is collecting info.."
 
 while true
 do
-    printf "${CYAN}System Info\n------------\n"
-    printf "${NC}$USER@$HOSTNAME - $(date)\n"
-    printf "Operating System: $PRETTY_NAME $(uname -m)\n"
-    printf "Logged in Users: $(who | wc -l)\n"
+    printf "${CYAN}System Info\n------------\n" > /tmp/memterm.out
+    printf "${NC}$USER@$HOSTNAME - $(date)\n" >> /tmp/memterm.out
+    printf "Operating System: $PRETTY_NAME $(uname -m)\n" >> /tmp/memterm.out
+    printf "Logged in Users: $(who | wc -l)\n" >> /tmp/memterm.out
     if [ "$(uptime | grep days)" == "" ]; then
         if [ "$(uptime | grep min)" == "" ]; then
             uptime=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+1)
             hours=$(echo $uptime | sed 's/:/\n/g' | head -n+1)
             minutes=$(echo $uptime | sed 's/:/\n/g' | tail -1)
-            printf "Uptime: $hours hours and $minutes minutes\n\n"
+            printf "Uptime: $hours hours and $minutes minutes\n\n" >> /tmp/memterm.out
         else
             uptime=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+1)
-            printf "Uptime: $(echo $uptime | head)\n\n"
+            printf "Uptime: $(echo $uptime | head)\n\n" >> /tmp/memterm.out
         fi
     else
         if [ "$(uptime | grep hours)" == "" ]; then
             days=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+1)
             uptime=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+2 | tail -1)
             minutes=$(echo $uptime | sed 's/:/\n/g' | tail -1| sed 's/ min//g')
-            printf "Uptime: $days and $minutes minutes\n\n"
+            printf "Uptime: $days and $minutes minutes\n\n" >> /tmp/memterm.out
 
         else
             days=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+1)
             uptime=$(uptime | sed 's/up/\n/g' | tail -1 | sed 's/,/\n/g' | head -n+2 | tail -1)
             hours=$(echo $uptime | sed 's/:/\n/g' | head -n+1)
             minutes=$(echo $uptime | sed 's/:/\n/g' | tail -1)
-            printf "Uptime: $days, $hours hours and $minutes minutes\n\n"
+            printf "Uptime: $days, $hours hours and $minutes minutes\n\n" >> /tmp/memterm.out
         fi
     fi
-    printf "${CYAN}Memory Info\n-----------\n${NC}"
+    printf "${CYAN}Memory Info\n-----------\n${NC}" >> /tmp/memterm.out
     # RAM usage
     ram=$(free -m | tail -n +2 | head -n +1)
     total_ram=$(echo $ram | cut -d' ' -f2) 
@@ -80,7 +85,7 @@ do
     ram_usg=$(echo "scale=3 ; (($used_ram+$used_ram_cache) / $total_ram)*100" | bc)
     ram_usg=$(echo "scale=0 ; $ram_usg/1" | bc)
     hashtags=$(print_bars "$ram_usg")
-    print_with_colours "$ram_usg" "RAM usage" "$hashtags"
+    print_with_colours "$ram_usg" "RAM usage" "$hashtags" >> /tmp/memterm.out
     
     # SWAP usage
     swap=$(free -m | tail -1)
@@ -90,9 +95,9 @@ do
         swap_usg=$(echo "scale=3 ; ($used_swap / $total_swap)*100" | bc)
         swap_usg=$(echo "scale=0 ; $swap_usg/1" | bc)
         hashtags=$(print_bars "$swap_usg")
-        print_with_colours "$swap_usg" "SWAP usage" "$hashtags"
+        print_with_colours "$swap_usg" "SWAP usage" "$hashtags" >> /tmp/memterm.out
     else
-        printf "${NC}No memory allocated as SWAP space.\n"    
+        printf "${NC}No memory allocated as SWAP space.\n" >> /tmp/memterm.out    
     fi
     # Disk usage
     clues=""
@@ -111,15 +116,17 @@ do
     done    
     disk_usg=$(echo $clues | rev | tr -d %)
     hashtags=$(print_bars "$disk_usg")
-    print_with_colours "$disk_usg" "Disk usage on '/'" "$hashtags"
+    print_with_colours "$disk_usg" "Disk usage on '/'" "$hashtags" >> /tmp/memterm.out
     
-    printf "\n${CYAN}Network Info\n-------------\n${NC}"
-    iface=$(ip route | grep via | cut -c13- | sed 's/ /\n/g' | sed -n 3p)
-    printf "Default Interface: "$iface"\n"
-    printf "Private IP: "$(ip a | grep $iface | grep inet | cut -c5- | sed 's/ /\n/g' | sed -n 2p)"\n"
-    printf "Default Gateway: "$(ip route | grep via | cut -c13- | sed 's/ /\n/g' | head -n+1)"\n"
+    printf "\n${CYAN}Network Info\n-------------\n${NC}" >> /tmp/memterm.out
+    iface=$(ip route | head -n+1 | grep -o "dev.*" | cut -d" " -f2)
+    printf "Default Interface: "$iface"\n" >> /tmp/memterm.out
+    printf "Private IP: "$(ip a show $iface | grep -o inet.* | head -n +1 | cut -d" " -f2)"\n" >> /tmp/memterm.out
+    printf "Default Gateway: "$( ip route | head -n+1 | grep -o "via.*" | cut -d" " -f2)"\n" >> /tmp/memterm.out
     pubip=$(curl -s ifconfig.me)
-    printf "Public IP: "$pubip
-    sleep 5
+    printf "Public IP: "$pubip >> /tmp/memterm.out
+	sleep 2
     tput clear
+    cat /tmp/memterm.out
+	rm /tmp/memterm.out
 done
